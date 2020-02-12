ba9a76ac34a675b1c42ea1525900176ae930eabf
LinkShen <a45154630@163.com>
2020-02-12 14:51:04 +0800

    start miscutils project and add scroll util

---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// config
+let scrollnum = 10;
+function loadConfig()
+{
+	scrollnum = vscode.workspace.getConfiguration("miscutils").get<number>("scrollnum", 10);
+}
+
+function scroll(line: number) {
+	let activeTextEditor = vscode.window.activeTextEditor;
+	if (!activeTextEditor) return;
+	let currentPosition = activeTextEditor.selection.active;
+    let moveToLine = currentPosition.line + line;
+    let documentLineCount = activeTextEditor.document.lineCount;
+    if (moveToLine > documentLineCount - 1) {
+        moveToLine = documentLineCount - 1;
+    }
+    if (moveToLine < 0) {
+        moveToLine = 0;
+    }
+    let moveToCharactor = activeTextEditor.document.lineAt(moveToLine).firstNonWhitespaceCharacterIndex;
+    let newPosition = new vscode.Position(moveToLine, moveToCharactor);
+    activeTextEditor.selection = new vscode.Selection(newPosition, newPosition);
+    activeTextEditor.revealRange(activeTextEditor.selection, vscode.TextEditorRevealType.InCenter);
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	loadConfig();
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(loadConfig));
+
+	context.subscriptions.push(vscode.commands.registerCommand('miscutils.scrolldown', () => {
+		scroll(scrollnum);
+	}));
+	context.subscriptions.push(vscode.commands.registerCommand('miscutils.scrolldown', () => {
+		scroll(scrollnum);
+	}));
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
